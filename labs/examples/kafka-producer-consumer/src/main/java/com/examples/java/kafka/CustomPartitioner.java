997c1c87617390111a58433ff7108a2fa9af280b
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2020-09-05 07:19:43 +0530

    training material updates

--- labs/examples/kafka-producer-consumer/src/main/java/com/examples/java/kafka/CustomPartitioner.java
+++ labs/examples/kafka-producer-consumer/src/main/java/com/examples/java/kafka/CustomPartitioner.java
@@ -18,7 +18,16 @@ public class CustomPartitioner implements Partitioner {
 	public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes,
 			Cluster cluster) {
 		Integer keyInt = Integer.parseInt(key.toString());
+		// Partitioning logic based on pre-defined partition count
 		int partition = keyInt % PARTITION_COUNT;
+
+		// 0 => 0,3,6,9
+		// 1 => 1,4,7
+		// 2 => 2,5,8
+		
+		// Partitioning logic based on dynamic partition count
+//		int partition = keyInt % cluster.partitionCountForTopic(topic);
+		
 		System.out.println("Sending message to partition - " + partition);
 		return partition;
 	}
